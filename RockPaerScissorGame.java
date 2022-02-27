070a71988609e5acca73098ed982315110dfba88
nidhisingh-19 <nidhidineshsingh123@gmail.com>
2022-02-27 17:18:26 +0530

    GameRPS

---
+++ RockPaerScissorGame.java
@@ -0,0 +1,47 @@
+import java.util.Random;
+import java.util.*;
+class RockPaerScissorGame
+{
+	public static void main(String args[])
+	{
+		Scanner sc = new Scanner(System.in);
+		//0 for rock
+		//1 for paper
+		//2 for scissor
+		
+		System.out.println("Enter 0 for rock, 1 for paper and 2 for scissor->");
+		int userInput = sc.nextInt();
+		
+
+		Random random = new Random();
+		int computerInput = random.nextInt(3);
+		
+		if(userInput == computerInput )
+		{
+			System.out.println("Draw");
+		}
+		else if(userInput == 0 && computerInput == 2 || userInput == 1 && computerInput == 0|| userInput == 2 && computerInput==1)
+		{
+			System.out.println("You Win !!");
+		}
+		else
+		{
+			System.out.println("Computer Win!");
+		}
+		System.out.println("Computer choice : "+ computerInput);
+       
+       		 if(computerInput == 0)
+        	{
+           		 System.out.println("Computer choice : Rock " + computerInput);
+        	}
+       		 else if(computerInput == 1)
+        	{
+            		System.out.println("Computer choice : Paper " + computerInput);
+        	}
+      		  else
+      	  	{
+            		System.out.println("Computer choice : Scissor " + computerInput);
+       		 }
+
+	}
+}
